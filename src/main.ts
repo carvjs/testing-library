3da3be828829bf3d343b9885efc8639f87ccb272
Sascha Tandel <s.tandel@kenoxa.de>
2020-07-06 00:03:22 +0200

    fix: re-exports

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ export { default as MockProvider } from './components/mock-provider.svelte'
 export * from './services/runtime'
 export * from './utils/render'
 
-export * from '@testing-library/dom'
+export {
+  createEvent,
+  getNodeText,
+  prettyDOM,
+  getQueriesForElement,
+  isInaccessible,
+  within,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/dom'
 export { screen, fireEvent, act, cleanup } from '@testing-library/svelte'
 export type { RenderResult, RenderOptions, FireFunction, FireObject } from '@testing-library/svelte'
 export { default as userEvent } from '@testing-library/user-event'
